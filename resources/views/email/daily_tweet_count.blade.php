6e6d2383df5f2b547ac7a5d6cddc5f608ade9cd9
gyumei <hannibal221b219@gmail.com>
2022-11-04 15:46:35 +0900

    first commit

---
+++ resources/views/email/daily_tweet_count.blade.php
@@ -0,0 +1,11 @@
+@component('mail::message')
+
+{{ $toUser->name }}さんこんにちは!
+
+昨日は{{ $count }}件のつぶやきが追加されましたよ!最新のつぶやきを見にいきましょう。
+
+@component('mail::button', ['url' => route('tweet.index')])
+つぶやきを見に行く
+@endcomponent
+
+@endcomponent
